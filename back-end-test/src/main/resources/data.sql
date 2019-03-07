0bc29b3b9a906ac48a17f3c0427bf3c725657ebe
Felipe Dierka <felipe.dierka@ciss.com.br>
2019-03-07 09:19:27 -0300

    Estrutura de banco.

---
+++ back-end-test/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO TEST.CATEGORY VALUES
+       (TEST.CATEGORY_SEQ.NEXTVAL, 'Alimentos'),
+       (TEST.CATEGORY_SEQ.NEXTVAL, 'Eletrodomésticos'),
+       (TEST.CATEGORY_SEQ.NEXTVAL, 'Móveis');
+
+
+INSERT INTO TEST.PRODUCT VALUES
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Arroz', 1),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Feijão', 1),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Aspirador de pó', 2),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Batedeira', 2),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Liquidificador', 2),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Sofá', 3),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Mesa', 3),
+       (TEST.PRODUCT_SEQ.NEXTVAL, 'Estante', 3);
