f67b04a130e31586f072d9ce9b6366bd88ae83a2
Julio Muller <juliodolago@gmail.com>
2019-07-31 20:38:51 -0300

    Started up project: included dependencies + 'hello world'

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div className="container">
+        <div className="text-center">
+          <h1>Hello, world!</h1>
+        </div>
+      </div>
+    )
+  }
+}
