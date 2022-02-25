492fe8abe367998169b15b857826e686b687e74e
Julio Muller <juliodolago@gmail.com>
2022-02-24 22:22:26 -0300

    fix: adjust linitng configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,17 @@
-/* eslint-env node */
 module.exports = {
   root: true,
-  extends: [
-    'react-app',
-    'lacussoft',
-    'lacussoft/react',
-    'lacussoft/typescript',
-  ],
-  ignorePatterns: [
-    'build/',
-    'dist/',
-    '!.babelrc.js',
-    '!commitlint.config.js',
-  ],
-  plugins: [
-    'jest',
-  ],
+  extends: ['react-app'],
+  plugins: ['prettier'],
+  ignorePatterns: ['build/', 'dist/', '!.babelrc.js', '!commitlint.config.js'],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'prettier/prettier': [
+      'warn',
+      {
+        printWidth: 100,
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
   },
 }
